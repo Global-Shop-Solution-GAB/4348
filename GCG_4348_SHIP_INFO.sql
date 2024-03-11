57445319e9c1429a17295bc3fb73482f6f759a37
Service Web <sw@gssmail.com>
2024-03-11 16:18:26 -0500

    Commit changes in C:\Websites\Git\emarlow\4348\GCG_4348_SHIP_INFO.sql

---
+++ GCG_4348_SHIP_INFO.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_4348_SHIP_INFO
+ * ========================================== */
+CREATE TABLE "GCG_4348_SHIP_INFO"(
+ "CUSTOMER" CHAR(6),
+ "LABEL" CHAR(10),
+ "PACKING_SLIP" CHAR(15),
+ "SHIPPER" CHAR(10));
+
+
