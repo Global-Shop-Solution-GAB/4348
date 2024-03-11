c11565069b4fdf189b1579546c9b3ca90e1da004
Service Web <sw@gssmail.com>
2024-03-11 16:18:26 -0500

    Commit changes in C:\Websites\Git\emarlow\4348\GCG_4348_LOCKED.sql

---
+++ GCG_4348_LOCKED.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_4348_LOCKED
+ * ========================================== */
+CREATE TABLE "GCG_4348_LOCKED"(
+ "SO_NUMBER" CHAR(7),
+ "PO_NUMBER" CHAR(1),
+ "DNLOADED" CHAR(1),
+ "LOCKED" CHAR(1));
+
+
