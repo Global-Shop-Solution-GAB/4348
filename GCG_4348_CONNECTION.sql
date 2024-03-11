a91bb3606b5d3085f7cfabffda430d428bf68b93
Service Web <sw@gssmail.com>
2024-03-11 16:18:26 -0500

    Commit changes in C:\Websites\Git\emarlow\4348\GCG_4348_CONNECTION.sql

---
+++ GCG_4348_CONNECTION.sql
@@ -0,0 +1,12 @@
+/* ==========================================
+ * TABLE: GCG_4348_CONNECTION
+ * ========================================== */
+CREATE TABLE "GCG_4348_CONNECTION"(
+ "NUM" CHAR(1),
+ "PROVIDER" CHAR(50),
+ "SERVER" CHAR(50),
+ "DATABASE" CHAR(50),
+ "USER_ID" CHAR(50),
+ "PASSWORD" CHAR(50));
+
+
